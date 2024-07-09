0adacf4065b6ed0283393dbabaa0ade315e082e0
diatsilva007 <diatsilva007@gmail.com>
2024-07-09 00:12:48 -0300

    Project Plann.er and his dependecies created

---
+++ src/app.tsx
@@ -0,0 +1,24 @@
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+    <div className="max-w-3xl w-full px-6 text-center space-y-10">
+      <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
+
+      <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+
+        <input type="text" placeholder="Para onde você vai?" />
+        <input type="text" placeholder="Quando?" />
+
+        <button>Continuar</button>
+
+      </div>
+
+      <p className="text-sm text-zinc-500">
+        Ao Planejar sua viagem pela plann.er você automaticamente concorda <br /> com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade.</a>
+      </p>
+    </div>
+
+    </div>
+  )
+}
+
