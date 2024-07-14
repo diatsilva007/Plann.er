68d6f9750312bf6ec89aaf544cc8075c06856f32
diatsilva007 <diatsilva007@gmail.com>
2024-07-14 15:54:16 -0300

    dependecies updated

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,46 +1,89 @@
-import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { ArrowRight, Calendar, MapPin, Settings2, X } from "lucide-react";
+import { useState } from "react";
+import { DateRange, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
 interface DestinationAndDateStepProps {
-    isGuestsInputOpen: boolean
-    closeGuestsInput: () => void
-    openGuestsInput: () => void
+  isGuestsInputOpen: boolean;
+  closeGuestsInput: () => void;
+  openGuestsInput: () => void;
 }
 
 export function DestinationAndDateStep({
-    closeGuestsInput,
-    openGuestsInput,
-    isGuestsInputOpen,
+  closeGuestsInput,
+  openGuestsInput,
+  isGuestsInputOpen,
 }: DestinationAndDateStepProps) {
-    return (
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+  const [eventStartsAndEndDates, setEventStartAndEndDates] = useState<DateRange | undefined>()
+
+  function openDatePicker() {
+    return setIsDatePickerOpen(true);
+  }
+
+  function closeDatePicker() {
+    return setIsDatePickerOpen(false);
+  }
+
+  return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-        
-        <div className='flex items-center gap-2 flex-1'>
-            <MapPin className='size-5 text-zinc-400'/>
-            <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+      <div className="flex items-center gap-2 flex-1">
+        <MapPin className="size-5 text-zinc-400" />
+        <input
+          disabled={isGuestsInputOpen}
+          type="text"
+          placeholder="Para onde você vai?"
+          className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+        />
+      </div>
+
+      <button
+        onClick={openDatePicker}
+        disabled={isGuestsInputOpen}
+        className="flex items-center gap-2 text-left"
+      >
+        <Calendar className="size-5 text-zinc-400" />
+        <span className=" text-lg text-zinc-400 w-40">Quando?</span>
+      </button>
+
+      {isDatePickerOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">
+                  Selecione a data
+                </h2>
+                <button type="button" onClick={closeDatePicker}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+            </div>
+
+            <DayPicker mode="range" selected={eventStartsAndEndDates} onSelect={setEventStartAndEndDates} />
+          </div>
         </div>
+      )}
 
-        <div className='flex items-center gap-2 '>
-            <Calendar className='size-5 text-zinc-400'/>
-            <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none"/>
-        </div>
+      <div className="w-px h-6 bg-zinc-800"></div>
 
-        <div className='w-px h-6 bg-zinc-800'></div>
-
-        {isGuestsInputOpen ? (
-
-            <button onClick={closeGuestsInput} className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>  
-            Alterar local/data
-            <Settings2 className='size-5'/>
-            </button>
-
-        ) : (
-
-            <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-                Continuar
-                <ArrowRight className='size-5 '/>
-            </button>
-
-    )}
+      {isGuestsInputOpen ? (
+        <button
+          onClick={closeGuestsInput}
+          className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+        >
+          Alterar local/data
+          <Settings2 className="size-5" />
+        </button>
+      ) : (
+        <button
+          onClick={openGuestsInput}
+          className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+        >
+          Continuar
+          <ArrowRight className="size-5 " />
+        </button>
+      )}
     </div>
-    )
+  );
 }
